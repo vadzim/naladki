5827655b582a1503d42f706ab7d5dae5a561d400
user <user@email>
2016-08-04 16:36:31 +0300

    choco

---
+++ choco_install.cmd
@@ -0,0 +1,5 @@
+rem @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+
+choco install -y 7zip atom chocolatey ext2fsd far firefox gimp googlechrome inkscape libreoffice nodejs notepadplusplus sublimetext3 sublimetext3.packagecontrol transmission vivaldi vlc
+
+rem audacity avastfreeantivirus virtualbox virtualbox.extensionpack 
